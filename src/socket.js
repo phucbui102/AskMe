058120f09b91b93153f973056fea178d4730deed
phucbui <laptaolap@gmail.com>
2025-05-13 10:29:16 +0700

    update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
+const socket = io('https://restfulapi-firstapp.onrender.com/', {
   withCredentials: true,
 });
 
